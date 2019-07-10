08c2d0a872b00d79d47960cf5e83cb576da90145
HeyuanMingong <njuwangzhi@gmail.com>
2019-07-10 09:42:23 +0800

    from local

--- data_process.py
+++ data_process.py
@@ -25,16 +25,13 @@ def arr_ave(arr, bs=1):
 ### observing the rewards of tested methods
 ### including: Random, Pretrained, PRPG, PR, IW, PR+IW
     
-DOMAIN = 'hopper'
+DOMAIN = 'navi_v1'
 p_model = 'saves/%s'%DOMAIN; p_output = 'output/%s'%DOMAIN
-
-#rewards = np.load(os.path.join(p_model, 'rewards_pretrained.npy'))
-#simple_plot([rewards])
 
 if DOMAIN in ['navi_v1', 'navi_v2', 'navi_v3']:
     navigation_domains()
 elif DOMAIN in ['reacher_v1', 'reacher_v2', 'reacher_v3',
-                'swimmer', 'hopper']:
+                'swimmer', 'hopper', 'cheetah']:
     mujoco_domains()  
 
 
